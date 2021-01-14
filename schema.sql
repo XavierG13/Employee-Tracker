a0be145033dc752bb72fdfab910300bf3ae2ee02
Xavier Guzman <guzmanxavi112@gmail.com>
2021-01-13 23:12:46 -0600

    created table for role

--- schema.sql
+++ schema.sql
@@ -1,9 +1,8 @@
 CREATE DATABASE employee_trackerDB;
-
 USE employee_trackerDB;
-
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR (30) NOT NULL,
     PRIMARY KEY (id)
 );
+CREATE TABLE
