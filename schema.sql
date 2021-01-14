9ccf76bb2242f313faf168fb52b8e8ab680e882b
Xavier Guzman <guzmanxavi112@gmail.com>
2021-01-13 23:00:14 -0600

    added files for project. created DB and table for department

---
+++ schema.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE employee_trackerDB;
+
+USE employee_trackerDB;
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR (30) NOT NULL,
+    PRIMARY KEY (id)
+);
