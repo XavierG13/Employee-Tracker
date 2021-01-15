941344364e9dcc90b16ee6663fd6ce2e61e12507
Xavier Guzman <guzmanxavi112@gmail.com>
2021-01-15 11:29:13 -0600

    updated schema and added multiple seeds to sql folder

--- schema.sql
+++ schema.sql
@@ -5,7 +5,7 @@ CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR (30) NOT NULL
 );
-CREATE TABLE job_role (
+CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(10, 4) NULL,
@@ -16,8 +16,8 @@ CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
-    job_role_id INT,
+    role_id INT,
     manager_id INT,
-    FOREIGN KEY (job_role_id) REFERENCES employee(id),
+    FOREIGN KEY (role_id) REFERENCES role(id),
     FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
