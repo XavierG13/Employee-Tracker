c42dc8dcf4061f744ce80444c11234e6aea17f42
Xavier Guzman <guzmanxavi112@gmail.com>
2021-01-15 11:18:31 -0600

    added schema for individual tables and added seeds sql to hold data

--- schema.sql
+++ schema.sql
@@ -1,8 +1,23 @@
+DROP DATABASE IF EXISTS employee_trackerDB;
 CREATE DATABASE employee_trackerDB;
 USE employee_trackerDB;
 CREATE TABLE department (
-    id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR (30) NOT NULL,
-    PRIMARY KEY (id)
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR (30) NOT NULL
 );
-CREATE TABLE
+CREATE TABLE job_role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(10, 4) NULL,
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES department(id)
+);
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    job_role_id INT,
+    manager_id INT,
+    FOREIGN KEY (job_role_id) REFERENCES employee(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
+);
