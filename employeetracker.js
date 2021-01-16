1a01ce55ba69876ccf770912b5d592690c8b5380
Xavier Guzman <guzmanxavi112@gmail.com>
2021-01-16 00:59:15 -0600

    created function that will run a search and prompt user to choose what they would like to do

--- employeetracker.js
+++ employeetracker.js
@@ -4,8 +4,8 @@ var inquirer = require("inquirer");
 var connection = mysql.createConnection({
   host: "localhost",
 
-  // Your port; if not 8080
-  port: 8080,
+  // Your port; if not 3306
+  port: 3306,
 
   // Your username
   user: "root",
@@ -17,4 +17,54 @@ var connection = mysql.createConnection({
 
 connection.connect(function (err) {
   if (err) throw err;
+  runSearch();
 });
+
+function runSearch() {
+  inquirer
+    .prompt({
+      name: "action",
+      type: "rawlist",
+      message: "What would you like to do?",
+      choices: [
+        "View All Employees",
+        "View All Employees by Department",
+        "View All Employees by Manager",
+        "Add Employee",
+        "Remove Employee",
+        "Update Employee Role",
+        "Update Employee Manager",
+      ],
+    })
+    .then(function (answer) {
+      switch (answer.action) {
+        case "View All Employees":
+          //   allEmployees();
+          break;
+
+        case "View All Employees by Department":
+          //   employeeDepartment();
+          break;
+
+        case "View All Employees by Manager":
+          //   employeeManager();
+          break;
+
+        case "Add Employee":
+          //   addEmployee();
+          break;
+
+        case "Remove Employee":
+          //   removeEmployee();
+          break;
+
+        case "Update Employee Role":
+          //   updateEmployee();
+          break;
+
+        case "Update Employee Manager":
+          //   updateManager();
+          break;
+      }
+    });
+}
